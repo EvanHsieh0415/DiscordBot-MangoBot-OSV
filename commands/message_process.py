6174c50acd1661f5f493999cf47ca67a4ba7c9ee
Mango <evan940415@gmail.com>
2021-03-14 15:58:13 +0800

    Update message_process.py

--- commands/message_process.py
+++ commands/message_process.py
@@ -1,10 +1,6 @@
-from os import pipe
-from typing import ChainMap
 import discord
 import json
-from discord import channel
 from discord.ext import commands
-from discord.ext.commands.core import command
 from core.classes import Cog_Extension
 
 with open('.\\settings\\message_process.json', mode='r', encoding='utf8') as MessageFile:

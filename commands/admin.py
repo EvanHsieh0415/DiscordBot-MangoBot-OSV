2f927cd47b2babe13f63b434f6a419bb2bf8227f
Mango <evan940415@gmail.com>
2021-03-25 20:48:46 +0800

    updata

--- commands/admin.py
+++ commands/admin.py
@@ -1,5 +1,5 @@
 import discord
-import json, time
+import json, time, os
 from discord.ext import commands
 from core.classes import Cog_Extension
 
@@ -30,6 +30,34 @@ class admin(Cog_Extension):
                 deleted =  await ctx.channel.purge(limit = count+3)
                 await ctx.send(repect_clean(wait_sec=wait_sec))
                 await ctx.send(f'[Admin Command] 已刪除{len(deleted)-3}條訊息')
+    
+    @commands.has_guild_permissions(administrator=True)
+    @commands.group()
+    async def set(self, ctx):
+        pass
 
+    @set.command()
+    async def event(self, ctx, channel_type:str, setting=None):
+        with open('.\\settings\\event.json', mode='r', encoding='utf8') as EventFile:
+            EventData = json.load(EventFile)
+        if setting == None:
+            index = EventData[str(ctx.guild.id)]['feedback'][channel_type]['channel']
+            await ctx.send(f'`{ctx.guild.id}/feedback/{channel_type}/channel` :  `{index}`')
+        else:
+            EventData[str(ctx.guild.id)]['feedback'][channel_type]['channel'] = setting
+            with open('.\\settings\\event.json', mode='w', encoding='utf8') as EventFile:
+                json.dump(EventData, EventFile, sort_keys=True, indent=4, ensure_ascii=False)
+            await ctx.send(f'edit `{ctx.guild.id}/feedback/{channel_type}/channel` :  `{setting}`')
+    
+    @set.command()
+    async def json(self, ctx):
+        for Filename in os.listdir('.\\settings'):
+            if Filename.endswith('.json'):
+                with open(f'.\\settings\\{Filename}', mode='r', encoding='utf8') as File:
+                    Data = json.load(File)
+                with open(f'.\\settings\\{Filename}', mode='w', encoding='utf8') as File:
+                    json.dump(Data, File, sort_keys=True, indent=4, ensure_ascii=False)
+                await ctx.send(f'json.dump {Filename}')
+        await ctx.send('json.dump all complete')
 def setup(bot):
     bot.add_cog(admin(bot))

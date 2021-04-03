5a66bdf1e598e8169850d8eb9140e1f752582cfe
Mango <evan940415@gmail.com>
2021-04-03 19:09:02 +0800

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -4,14 +4,29 @@ from core.classes import Cog_Extension
 
 class help(Cog_Extension):
     @commands.command()
-    async def help_(self, ctx, page:int):
-        maxpage = 2
-        if page == 1:
-            help_type = 'Game'
-        embed=discord.Embed(title=f'page: {page}', description=help_type)
-        embed.set_author(name='Help')
-        embed.set_footer(text=f'Page {page}/{maxpage}')
-        await ctx.send(embed=embed)
+    async def menu(self, ctx, page:int=0):
+        if page == 0:
+            embed=discord.Embed(title='Help Menu', description='幫助菜單(?)')
+            embed.add_field(name='1. Game 遊戲大廳', value='Page: 1', inline=True)
+            embed.add_field(name='2. Search 搜尋系統', value='Page: 2', inline=True)
+            embed.add_field(name='3. Information 資訊查詢', value='Page: 3', inline=True)
+            embed.add_field(name='4. Verify 驗證系統', value='Page: 4', inline=True)
+            embed.add_field(name='5. Economy 經濟系統', value='Page: 5', inline=True)
+            embed.add_field(name='6. Feedback 回饋', value='Page: 6', inline=True)
+            await ctx.send(embed=embed)
+        elif page == 1:
+            pass
+        elif page == 2:
+            pass
+        elif page == 3:
+            pass
+        elif page == 4:
+            pass
+        elif page == 5:
+            pass
+        elif page == 6:
+            pass
+
 
     @commands.command()
     async def call_owner(self, ctx, *, msg:str=None):
